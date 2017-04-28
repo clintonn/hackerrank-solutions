50ca13931649fcf4e59fd544f8f672e1167e7d83
Clinton Nguyen <clinton.ngyn@gmail.com>
2017-04-27 20:07:46 -0400

    Finish breaking the records 🎖

--- algorithms/implementation/breaking-the-records.js
+++ algorithms/implementation/breaking-the-records.js
@@ -1,4 +1,52 @@
-process.stdin.resume();
+
+ss.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function getRecord(s){
+  var worst = s[0], best = s[0]
+  var worstBroken = 0, bestBroken = 0
+  s.slice(1).forEach(score => {
+    if (score > best) {
+      best = score
+      bestBroken++
+    } else if (score < worst) {
+      worst = score
+      worstBroken++
+    } else {
+      return
+    }
+  })
+  return [bestBroken, worstBroken]
+}
+
+function main() {
+    var n = parseInt(readLine());
+    s = readLine().split(' ');
+    s = s.map(Number);
+    var result = getRecord(s);
+    console.log(result.join(" "));
+
+}
+.stdin.resume();
 process.stdin.setEncoding('ascii');
 
 var input_stdin = "";
